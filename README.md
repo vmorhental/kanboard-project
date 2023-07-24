011049729e47e7b4ced08e318e6fb2670e1181fd
vmorhental <valera.morhental@gmail.com>
2023-07-25 00:00:36 +0300

    Added possibility to run different profiles Added possibility to run tests on different browsers Readme file was updated with according commands

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # kanboard-project
+- To run tests on Chrome headless please use 
+  #### mvn clean test -Dbrowser=chrome -Dheadless=true
+- To run tests on Firefox not headless please use 
+  #### mvn clean test -Dbrowser=firefox -Dheadless=false
+- To run tests on Chrome not headless please use 
+  #### mvn clean test -Dbrowser=chrome -Dheadless=false
+- To run only API tests please use
+  #### mvn clean test -PAPI_tests_run
+- To run only UI tests please use
+  #### mvn clean test -PUI_tests_run -Dbrowser=chrome -Dheadless=false
+- To run all tests please use
+  #### mvn clean test -PFull_run -Dbrowser=chrome -Dheadless=false 
--- src/test/java/ui/tests/BaseTest.java
+++ src/test/java/ui/tests/BaseTest.java
@@ -6,15 +6,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import static utils.EnvProperties.BASE_URL;
 
 public class BaseTest {
     @BeforeMethod
     public void setUp() {
-        Configuration.browser = "chrome";
+        String browser = System.getProperty("browser");
+        String headless = System.getProperty("headless");
+        Configuration.browser = browser;
         Configuration.baseUrl = BASE_URL;
+        if (headless.equals("true")) {
+            Configuration.headless = true;
+        }
+        else Configuration.headless = false;
     }
+//    @Parameters ("instanceName")
+//    @BeforeTest
+//    public static void setupInstance (String instanceName){
+//        switch (instanceName){
+//            case "kanban1":{
+//
+//            }
+//        }
+//    }
 
     @AfterMethod(alwaysRun = true)
     public void closeUp() {
